360efcccde4464ff724a563c8de5fde6a04c5333
ashprice <vofan@tuta.io>
2020-06-29 17:11:19 +0100

    meh

--- installer/arsch.sh
+++ installer/arsch.sh
@@ -42,7 +42,7 @@ else
     VG=vg0
     if [ -n VG ] ; then
         lvs|grep ${VG}|awk -F\  '{print $1}'|xargs -n1 -I{} -- lvremove --force -y {} || /bin/true
-        vgremove -y -f ${VG}
+        vgremove -y -f ${VG} || /bin/true
     fi
     pvremove -y -f ${device}
 fi
